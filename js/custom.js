4e4d40c374c947bb4a6ae0ad1648c195d3bea897
Mahbub-diu <mahbub.mediasoft@gmail.com>
2022-10-04 11:51:25 +0600

    update home without footer

--- js/custom.js
+++ js/custom.js
@@ -375,3 +375,43 @@ $('.main-slider-wrapper').slick({
   infinite: true,
 });
 // carrer page slider end here
+
+// feature department slider for home page
+
+$('.fd-wrapper').slick({
+  dots: false,
+  infinite: false,
+  slidesToShow: 8,
+  slidesToScroll: 2,
+  autoplay: true,
+  arrows: true,
+  infinite: true,
+  prevArrow:
+    "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
