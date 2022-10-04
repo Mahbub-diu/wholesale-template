a2441b4d304f1c14bd768d29ff004cbc4d833def
Mahbub-diu <mahbub.mediasoft@gmail.com>
2022-10-04 12:42:24 +0600

    update testimonial

--- js/custom.js
+++ js/custom.js
@@ -415,3 +415,42 @@ $('.fd-wrapper').slick({
     },
   ],
 });
+
+// testimonial slider start from here
+$('.testimonial-wrapper').slick({
+  dots: false,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: true,
+  infinite: true,
+  prevArrow:
+    "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
