e4793c5db6e94aa5840275271d5b4dfa7a30f667
Mahbub-diu <mahbub.mediasoft@gmail.com>
2022-10-25 16:39:05 +0600

    update recipe page

--- js/custom.js
+++ js/custom.js
@@ -471,3 +471,40 @@ function decrementValue() {
 
   document.getElementById('number').value = value;
 }
+$('.recipe-sm-slider-wrapper').slick({
+  dots: false,
+  infinite: false,
+  slidesToShow: 8,
+  slidesToScroll: 2,
+  autoplay: true,
+  arrows: true,
+  infinite: true,
+  prevArrow:
+    "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
