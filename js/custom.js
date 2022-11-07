678e9df963f9b84d90685932dee18726531972b0
Mahbub-diu <mahbub.mediasoft@gmail.com>
2022-11-07 18:48:56 +0600

    update membersip

--- js/custom.js
+++ js/custom.js
@@ -540,3 +540,48 @@ $('.slider-nav').slick({
 $('.promotion-toggle-btn').click(function () {
   $('.offer-dropdown-main').toggleClass('d-none');
 });
+
+$('.brand-slider-wrapper').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 8,
+  slidesToScroll: 2,
+  autoplay: true,
+  arrows: true,
+  infinite: true,
+  prevArrow:
+    "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
